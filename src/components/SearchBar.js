2f2a5b4f1be9a49d7980c29f578c0bf6449f8025
Oleg Lysenok <msk.lysenok@gmail.com>
2019-11-01 17:01:17 +0300

    added search relations

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,26 @@ class SearchBar extends React.Component {
   //CONTROLLED
   state = { term: "" };
 
+  /* ОШИБКА, потому что this не обращается к объекту лекция 84
   onFormSubmit(event) {
     event.preventDefault();
-  }
 
+    console.log(this.state.term);
+  }
+*/
+  // добавление стрелочной функции решает проблему
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    //console.log(this.state.term);
+    //передадим пропс из App через this - func component в class component
+    this.props.usersSubmit(this.state.term);
+  };
+
+  /* альтернативный метод onSubmit
+  <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
+  <form onSubmit={this.onFormSubmit} className="ui form">
+*/
   render() {
     return (
       <div className="ui segment">
@@ -17,7 +33,7 @@ class SearchBar extends React.Component {
             <input
               type="text"
               value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              onChange={event => this.setState({ term: event.target.value })}
             />
           </div>
         </form>
