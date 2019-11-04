e9bfa7c35a7b874f1898f856bfc365e064fe12a7
Oleg Lysenok <msk.lysenok@gmail.com>
2019-11-04 13:53:22 +0300

    UNSPLASH SEARCH ADDED

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 78215cfae8283e79a2c5f45027e11d0bb9bb3701f3b28675be97fe685c0d9949"
+      }
+    });
   }
 
   render() {
